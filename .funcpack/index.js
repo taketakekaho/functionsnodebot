52a91e9e7d6b7e50de344d1ab8012b0da05ea3c5
taketakekaho <kahori_takeda@change-jp.com>
2018-06-19 16:14:07 +0900

    テンプレートをコンパイル

---
+++ .funcpack/index.js
@@ -0,0 +1,184 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = {
+    "messages": __webpack_require__(1)
+}
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/*----------------------------------------------------------------------------------------
+* Azure Functions bot templates use Azure Functions Pack for optimal performance, get 
+* familiar with Azure Functions Pack at https://github.com/Azure/azure-functions-pack
+
+* For more information about this template visit http://aka.ms/azurebots-node-qnamaker
+* ---------------------------------------------------------------------------------------- */
+
+
+var builder = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"botbuilder\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+var botbuilder_azure = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"botbuilder-azure\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+var builder_cognitiveservices = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"botbuilder-cognitiveservices\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+var path = __webpack_require__(2);
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
+
+var tableName = 'botdata';
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+var bot = new builder.UniversalBot(connector);
+bot.localePath(path.join(__dirname, './locale'));
+bot.set('storage', tableStorage);
+
+// Recognizer and and Dialog for preview QnAMaker service
+var previewRecognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+                knowledgeBaseId: process.env.QnAKnowledgebaseId, 
+    authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey});
+
+var basicQnAMakerPreviewDialog = new builder_cognitiveservices.QnAMakerDialog({
+    recognizers: [previewRecognizer],
+                defaultMessage: 'No match! Try changing the query terms!',
+                qnaThreshold: 0.3}
+);
+
+bot.dialog('basicQnAMakerPreviewDialog', basicQnAMakerPreviewDialog);
+
+// Recognizer and and Dialog for GA QnAMaker service
+var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+    knowledgeBaseId: process.env.QnAKnowledgebaseId,
+    authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey,
+    endpointHostName: process.env.QnAEndpointHostName
+});
+
+var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
+    recognizers: [recognizer],
+    defaultMessage: 'No match! Try changing the query terms!',
+    qnaThreshold: 0.3
+}
+);
+
+bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
+
+bot.dialog('/', //basicQnAMakerDialog);
+[
+    function (session){
+        var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
+        var qnaAuthKey = process.env.QnAAuthKey || process.env.QnASubscriptionKey; // Backward compatibility with QnAMaker (Preview)
+        var endpointHostName = process.env.QnAEndpointHostName;
+        
+        // QnA Subscription Key and KnowledgeBase Id null verification
+        if ((qnaAuthKey == null || qnaAuthKey == '') || (qnaKnowledgebaseId == null || qnaKnowledgebaseId == ''))
+            session.send('Please set QnAKnowledgebaseId, QnAAuthKey and QnAEndpointHostName (if applicable) in App Settings. Learn how to get them at https://aka.ms/qnaabssetup.');
+        else {
+            if (endpointHostName == null || endpointHostName == '')
+                // Replace with Preview QnAMakerDialog service
+                session.replaceDialog('basicQnAMakerPreviewDialog');
+            else
+                // Replace with GA QnAMakerDialog service
+                session.replaceDialog('basicQnAMakerDialog');
+        }       
+    }
+]);
+
+if (useEmulator) {
+    var restify = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"restify\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());    
+} else {
+    module.exports = connector.listen();
+}
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+module.exports = require("path");
+
+/***/ })
+/******/ ]);
